2f844de22101a75a047d6924c19db2ff253fd243
MrFrost-Nv27 <nazruelyourgoodfriend@gmail.com>
2023-11-17 12:47:29 +0700

    view permohonan otw

--- views/panel/staff/index.php
+++ views/panel/staff/index.php
@@ -1,41 +1,43 @@
-<div class="row dashboard">
-    <div class="col l3 m6 s12">
-        <div class="staf">
-            <div class="icon"><i class="material-icons">email</i></div>
-            <div class="card center">
-                <h5>Surat Keterangan Usaha</h5>
-                <h1>0</h1>
-                <a class="waves-effect waves-light btn">Buat</a>
+<div class="staf">
+    <div class="row dashboard">
+        <div class="col l3 m6 s12">
+            <div class="card-item">
+                <div class="icon"><i class="material-icons">email</i></div>
+                <div class="card center">
+                    <h5>Surat Keterangan Usaha</h5>
+                    <h1>0</h1>
+                    <a class="waves-effect waves-light btn">Buat</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col l3 m6 s12">
-        <div class="staf">
-            <div class="icon"><i class="material-icons">email</i></div>
-            <div class="card center">
-                <h5>Surat Pengantar</h5>
-                <h1>0</h1>
-                <a class="waves-effect waves-light btn">Buat</a>
+        <div class="col l3 m6 s12">
+            <div class="card-item">
+                <div class="icon"><i class="material-icons">email</i></div>
+                <div class="card center">
+                    <h5>Surat Pengantar</h5>
+                    <h1>0</h1>
+                    <a class="waves-effect waves-light btn">Buat</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col l3 m6 s12">
-        <div class="staf">
-            <div class="icon"><i class="material-icons">email</i></div>
-            <div class="card center">
-                <h5>Surat Keterangan</h5>
-                <h1>0</h1>
-                <a class="waves-effect waves-light btn">Buat</a>
+        <div class="col l3 m6 s12">
+            <div class="card-item">
+                <div class="icon"><i class="material-icons">email</i></div>
+                <div class="card center">
+                    <h5>Surat Keterangan</h5>
+                    <h1>0</h1>
+                    <a class="waves-effect waves-light btn">Buat</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col l3 m6 s12">
-        <div class="staf">
-            <div class="icon"><i class="material-icons">email</i></div>
-            <div class="card center">
-                <h5>Surat Catatan Kepolisian</h5>
-                <h1>0</h1>
-                <a class="waves-effect waves-light btn">Buat</a>
+        <div class="col l3 m6 s12">
+            <div class="card-item">
+                <div class="icon"><i class="material-icons">email</i></div>
+                <div class="card center">
+                    <h5>Surat Catatan Kepolisian</h5>
+                    <h1>0</h1>
+                    <a class="waves-effect waves-light btn">Buat</a>
+                </div>
             </div>
         </div>
     </div>
---
+++ views/panel/staff/permohonan.php
@@ -0,0 +1,106 @@
+<div class="staf">
+    <div class="row">
+        <div class="col l8 offset-l2 m12">
+            <div class="card">
+                <div class="row">
+                    <div class="col s12">
+                        <ul class="tabs tabs-fixed-width">
+                            <li class="tab col s3"><a href="#test1">Permohonan Berjalan</a></li>
+                            <li class="tab col s3"><a href="#test2">Cetak Surat</a></li>
+                            <li class="tab col s3"><a href="#test3">Riwayat Permohonan</a></li>
+                        </ul>
+                    </div>
+                    <div class="content" style="padding: 1rem;">
+                        <div id="test1" class="col s12">
+                            <table class="datatables-init striped highlight">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Item Name</th>
+                                        <th>Item Price</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>Alvin</td>
+                                        <td>Eclair</td>
+                                        <td>$0.87</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Alan</td>
+                                        <td>Jellybean</td>
+                                        <td>$3.76</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jonathan</td>
+                                        <td>Lollipop</td>
+                                        <td>$7.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div id="test2" class="col s12">
+                            <table class="datatables-init striped highlight">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Item Name</th>
+                                        <th>Item Price</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>Alvin</td>
+                                        <td>Eclair</td>
+                                        <td>$0.87</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Alan</td>
+                                        <td>Jellybean</td>
+                                        <td>$3.76</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jonathan</td>
+                                        <td>Lollipop</td>
+                                        <td>$7.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div id="test3" class="col s12">
+                            <table class="datatables-init striped highlight">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Item Name</th>
+                                        <th>Item Price</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td>Alvin</td>
+                                        <td>Eclair</td>
+                                        <td>$0.87</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Alan</td>
+                                        <td>Jellybean</td>
+                                        <td>$3.76</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jonathan</td>
+                                        <td>Lollipop</td>
+                                        <td>$7.00</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
